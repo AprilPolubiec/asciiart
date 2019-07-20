d4bcdaec2c7eb64c1a718ddc384e5d798f899fb7
April Polubiec <aprilpolubiec@gmail.com>
2019-07-20 17:37:16 -0500

    Cleaned up, final product

--- project.rb
+++ project.rb
@@ -1,58 +1,68 @@
+ASCII_SCALE = '`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$'
+
+def successAlert
+    puts "Success!"
+end
+
+def displayImageSize(image)
+    puts "Image size: " + image.dimensions.to_s
+end
+
+def constructBrightnessMatrix(tuple_matrix)
+    brightness_matrix = []
+    tuple_matrix.each {|row|
+        new_row = []
+        row.each {|rgb_val|
+            new_row.push((rgb_val[0]+rgb_val[1]+rgb_val[2])/3)
+        }
+        brightness_matrix.push(new_row)
+    }
+    return brightness_matrix
+end
+
+def createAsciiArt(brightness_matrix)
+    ascii_matrix = ''
+    brightness_matrix.each {|row|
+        new_row = ''
+        row.each {|brightness_value|
+            2.times do
+                new_row += ASCII_SCALE[convertToASCII(brightness_value.to_f/255*65-1)]
+            end
+        }
+        ascii_matrix += new_row
+        ascii_matrix += "\n"
+    }
+    
+    return ascii_matrix
+end
+
 def convertToASCII(value)
     ((value/255)*65)-1
 end
 
 
 require 'mini_magick'
+
+puts "Loading image..."
 image = MiniMagick::Image.open("Headshot.jpg")
 
-puts "Successfully loaded image!"
-width = image.dimensions[0]
-height = image.dimensions[1]
-
-puts "Image size: " + image.dimensions.to_s
-
+successAlert
+displayImageSize(image)
 
 puts "Constructing pixel matrix..."
 RGB_tuple_matrix = image.get_pixels
-puts "Success!"
+successAlert
 
 puts "Constructing brightness matrix..."
-
-brightness_matrix = []
-
-
-#Loop to populate brightness matrix
-
-RGB_tuple_matrix.each {|row|
-    new_row = []
-    row.each {|rgb_val|
-        new_row.push((rgb_val[0]+rgb_val[1]+rgb_val[2])/3)
-    }
-    brightness_matrix.push(new_row)
-}
+brightness_matrix = constructBrightnessMatrix(RGB_tuple_matrix)
+successAlert
 
 
-puts "Success!"
 
 
-asciiScale = '`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$'
-ascii_matrix = ''
+puts "Creating ASCII art..."
+asciiArt = createAsciiArt(brightness_matrix)
+successAlert
 
-brightness_matrix.each {|row|
-    new_row = ''
-    row.each {|brightness_value|
-       2.times do
-           new_row += asciiScale[convertToASCII(brightness_value.to_f/255*65-1)]
-        end
-    }
-    ascii_matrix += new_row
-    ascii_matrix += "\n"
-}
+print asciiArt
 
-puts "Success!"
-print ascii_matrix
-
-#
-#print ascii_matrix
-#
